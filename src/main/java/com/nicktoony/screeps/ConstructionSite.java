13f3f1f969d5b0ec0d8893753d51d84bea8af650
Nick Hope <nick@gospelware.co.uk>
2015-08-10 15:21:35 +0100

    Added all methods/fields for Room API.

--- src/main/java/com/nicktoony/screeps/ConstructionSite.java
+++ src/main/java/com/nicktoony/screeps/ConstructionSite.java
@@ -1,9 +1,11 @@
 package com.nicktoony.screeps;
 
+import com.nicktoony.screeps.global.ScreepsObject;
+
 /**
  * Created by nick on 02/08/15.
  */
-public class ConstructionSite {
+public class ConstructionSite extends ScreepsObject {
     public String id;
     public int progress;
     public int progressTotal;
--- src/main/java/com/nicktoony/screeps/Room.java
+++ src/main/java/com/nicktoony/screeps/Room.java
@@ -1,10 +1,11 @@
 package com.nicktoony.screeps;
 
-import com.nicktoony.screeps.global.FindTypes;
-import com.nicktoony.screeps.global.GlobalVariables;
-import com.nicktoony.screeps.global.ResponseTypes;
-import com.nicktoony.screeps.global.StructureTypes;
+import com.nicktoony.screeps.global.*;
+import com.nicktoony.screeps.helpers.SurvivalInfo;
+import com.nicktoony.screeps.helpers.Targetable;
 import com.nicktoony.screeps.structures.Controller;
+import com.nicktoony.screeps.structures.Storage;
+import com.sun.istack.internal.Nullable;
 import org.stjs.javascript.Array;
 import org.stjs.javascript.Map;
 import org.stjs.javascript.annotation.STJSBridge;
@@ -19,10 +20,37 @@ public abstract class Room {
     public Controller controller;
     public int energyAvailable;
     public int energyCapacityAvailable;
+    public ModeTypes mode;
+    private Storage storage;
+    private SurvivalInfo survivalInfo;
 
-    public abstract Array<?> find(FindTypes type, Map parameters);
+    public abstract ResponseTypes createConstructionSite(int x, int y, StructureTypes type);
 
-    public abstract Array<Map<String, Object>>  findPath(RoomPosition pos, RoomPosition pos1, Map<String, Object> strings);
+    public abstract ResponseTypes createConstructionSite(RoomPosition position, StructureTypes type);
 
-    public abstract ResponseTypes createConstructionSite(int x, int y, StructureTypes structureRoad);
+    public abstract ResponseTypes createFlag(int x, int y, @Nullable String name, @Nullable ColorTypes color);
+
+    public abstract ResponseTypes createFlag(RoomPosition position, @Nullable String name, @Nullable ColorTypes color);
+
+    public abstract Array<? extends ScreepsObject> find(FindTypes type, Map<String, Object> options);
+
+    public abstract RoomDirectionTypes findExitTo(Room room);
+
+    public abstract RoomDirectionTypes findExitTo(String roomName);
+
+    public abstract Array<Map<String, Object>>  findPath(RoomPosition fromPos, RoomPosition toPos, @Nullable Map<String, Object> options);
+
+    public abstract RoomPosition getPositionAt(int x, int y);
+
+    public abstract Array<Map<String, Object>> lookAt(int x, int y);
+
+    public abstract Array<Map<String, Object>> lookAt(Targetable targetable);
+
+    public abstract Array<Map<String, Object>> lookAtArea(int top, int left, int bottom, int right);
+
+    public abstract Array<? extends ScreepsObject> lookForAt(LookTypes type, int x, int y);
+
+    public abstract Array<? extends ScreepsObject> lookForAt(LookTypes type, Targetable targetable);
+
+    public abstract Map<String, Map<String, Object>> lookForAtArea(LookTypes type, int top, int left, int bottom, int right);
 }
