6f7ef65d9dd6b079e8fa22a5ca9334d48ee5c4f9
NickToony <nicktoony@gmail.com>
2015-08-13 00:14:58 +0100

    ConstructionManager automatically builds extensions.

--- src/main/java/com/nicktoony/scrAI/Managers/ConstructionManager.java
+++ src/main/java/com/nicktoony/scrAI/Managers/ConstructionManager.java
@@ -5,8 +5,12 @@ import com.nicktoony.helpers.LodashCallback1;
 import com.nicktoony.scrAI.Controllers.RoomController;
 import com.nicktoony.scrAI.World.Tasks.TaskBuild;
 import com.nicktoony.screeps.ConstructionSite;
+import com.nicktoony.screeps.RoomPosition;
 import com.nicktoony.screeps.global.FindTypes;
 import com.nicktoony.screeps.global.GlobalVariables;
+import com.nicktoony.screeps.global.ResponseTypes;
+import com.nicktoony.screeps.global.StructureTypes;
+import com.nicktoony.screeps.structures.Spawn;
 import org.stjs.javascript.Array;
 import org.stjs.javascript.Global;
 import org.stjs.javascript.Map;
@@ -21,7 +25,8 @@ public class ConstructionManager extends Manager {
 
     @Override
     protected void init() {
-
+        memory.$put("layer", 3);
+        memory.$put("state", 0);
     }
 
     public void update() {
@@ -40,5 +45,76 @@ public class ConstructionManager extends Manager {
                 return true;
             }
         }, this);
+
+
+        // no build sites?
+        if (foundSites.$length() == 0) {
+            Spawn spawn = roomController.getSpawnsManager().getSpawns().$get(0);
+            if (spawn == null) return;
+
+            int layer = (Integer) memory.$get("layer");
+            int state = (Integer) memory.$get("state");
+            Global.console.log("LAYER: " + layer);
+
+            int topLeftX = spawn.pos.x-layer;
+            int topLeftY = spawn.pos.y-layer;
+
+            // Left side
+            if (state == 0 && tryToBuildSpawn(topLeftX, topLeftY,topLeftX, topLeftY + layer * 2)) {
+                memory.$put("state", state);
+                return;
+            } else if (state == 0) {
+                state = 1;
+            }
+            // Right side
+            if (state == 1 && tryToBuildSpawn(topLeftX + layer * 2, topLeftY,topLeftX + layer * 2, topLeftY + layer * 2)) {
+                memory.$put("state", state);
+                return;
+            } else if (state == 1) {
+                state = 2;
+            }
+            // Top side
+            if (state == 2 && tryToBuildSpawn(topLeftX, topLeftY,topLeftX + layer * 2, topLeftY)) {
+                memory.$put("state", state);
+                return;
+            } else if (state == 2) {
+                state = 3;
+            }
+            // Bottom side
+            if (state == 3 && tryToBuildSpawn(topLeftX, topLeftY + layer * 2,topLeftX + layer * 2, topLeftY + layer * 2)) {
+                memory.$put("state", state);
+                return;
+            } else if (state == 3) {
+                state = 4;
+            }
+
+            layer += 2;
+            state = 0;
+            // If gone beyond 10, that's not good
+            if (layer > 10) layer = 3;
+            memory.$put("layer", layer);
+            memory.$put("state", state);
+        }
+    }
+
+    private boolean tryToBuildSpawn(int fromX, int fromY, int toX, int toY) {
+        for (int x = fromX; x <= toX; x += 2) {
+            for (int y = fromY; y <= toY; y+= 2) {
+                RoomPosition position = new RoomPosition(x, y, roomController.getRoom().name);
+                Array objects = position.lookFor("terrain");
+                if (objects.$length() > 0 && objects.$get(0) == "plain") {
+                    ResponseTypes created = position.createConstructionSite(StructureTypes.STRUCTURE_EXTENSION);
+                    if (created == ResponseTypes.OK) {
+                        return true;
+                    } else if (created == ResponseTypes.ERR_RCL_NOT_ENOUGH) {
+                        return true;
+                    } else {
+                        // continue and try again
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
